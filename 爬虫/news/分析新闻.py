ca3bd3f1a9d2d12ab59534756248604e10cc07f8
ypwhs <915505626@qq.com>
2015-03-28 23:44:42 +0800

    master
    分析新闻

---
+++ 爬虫/news/分析新闻.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+__author__ = 'yangpeiwen'
+
+import jieba
+import jieba.analyse
+import os
+import re
+
+jieba.enable_parallel(4)
+jieba.analyse.set_stop_words("stop_words.txt")
+# jieba.analyse.set_idf_path("idf.txt")
+path = "跌"
+
+
+def fenci():
+    for parent, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            if filename.find("txt") != -1:
+                wb = open(path + "/" + filename).read()
+                wb = re.sub(r'http.+', '', wb)
+                tags = jieba.analyse.extract_tags(wb, topK=10)
+                print(",".join(tags))
+                # , withWeight=True
+                # for tag in tags:
+                #     print("tag: %s\t\t weight: %f" % (tag[0], tag[1]))
+                #     break
+                # break
+
+fenci()
--- 爬虫/下载树莓派图片.py
+++ 爬虫/下载树莓派图片.py
@@ -5,7 +5,7 @@ from urllib import *
 import os
 import socket
 
-url = "http://192.168.155.3/img/"
+url = "http://192.168.191.2/img/"
 socket.setdefaulttimeout(300)
 html = urlopen(url).read()
 soup = BeautifulSoup(html)
@@ -13,6 +13,6 @@ imgs = soup.findAll("img")
 for img in imgs:
     print img['src']
     url2 = url + img['src']
-    filepath = "/Users/yangpeiwen/Desktop/img/" + img['src']
+    filepath = "/Volumes/YPW-M-1/img/" + img['src']
     if not os.path.exists(filepath):
         urlretrieve(url2.encode('utf-8'), filepath)
