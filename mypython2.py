b760ded6c120fe082e9ec4bc1dfa0135e44ea4bf
ypwhs <915505626@qq.com>
2015-05-04 20:14:18 +0800

    master

---
+++ mypython2.py
@@ -0,0 +1,84 @@
+# coding=utf-8
+import math
+# Python入门练习
+# a = 1
+# b = 2
+# print a+b
+
+# 运算符 + - * / 整除// 求余% 乘方**
+# a = 3
+# b = 5
+# print a**b
+# print b**a
+# print a//b
+# print b//a
+
+# a = input()
+# b = input()
+# print a/b
+# print a//b
+
+# print 1j*1j
+# ij表示-1的平方根
+
+# math库的使用
+# a = [1, 2, 3, 4, 5, 6, 7]
+# for i in a:
+#     if math.ceil(i):
+#         print i
+
+# 字符串长度
+# print len('qwert')
+
+# # 字符串拼接
+# print 'hot' + 'dog'
+# print 10 * 'hot'
+
+# 列出模块中的函数
+# print dir(math)
+# Python 内置函数清单
+# print dir(__builtins__)
+# # 打印文档字符串
+# print(bin.__doc__)
+
+# print float(3)
+
+# print round(7.5), round(8.5), int(8.5), round(8.4)
+
+# y = 'cat'
+# print(y)
+
+# 多重赋值
+# x, y, z = 1, 'cat', 4.0
+# print x, y, z
+
+# 交换变量的值
+# a, b = 3, 5
+# a, b = b, a
+# print a, b
+
+# 从键盘读取字符
+# test = input()
+# print test
+
+# test1 = raw_input()
+# print 'Hello' + ' '+ test1.capitalize() + ' !'
+# 删除开头和末尾的空白字符
+# test = raw_input().strip()
+# print test
+
+# 从键盘读取字符转换成数字
+# a = raw_input()
+# print 2 * a
+# print 2 * int(a)
+
+# 打印字符串
+# print 'lucky', 'dog'
+
+# Bool逻辑 与或非相等不相等
+# print False == False
+# print False != True
+# print False and True
+# print False and False
+# print False or True
+# print not False
