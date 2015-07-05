2b2845aba15a6292c678496814b5bc45914ca2bc
ypw <915505626@qq.com>
2015-07-05 19:01:07 +0800

    1

--- xuanke.py
+++ xuanke.py
@@ -1,23 +1,36 @@
+# coding: utf-8
 __author__ = 'ypw'
 
 import time
 import requests
+
+yes = False
+i = 0
 s = requests.session()
 url = "http://10.0.40.216/bgsxk/xk/LoginToXk"
-postdata = {"USERNAME": "你的学号", "PASSWORD": "你的密码"}
+postdata = {"USERNAME": "xuehao", "PASSWORD": "mima"}
 r = s.post(url, params=postdata)
 s.get("http://10.0.40.216/bgsxk/xk/getXkInfo?jx0502zbid=97&jx0502id=50")
 # 历史类,其他类请自行抓包
-i = 0
+
+def shua(inurl):
+    global yes, i
+    r2 = s.get(inurl, timeout=5)
+    # 西方音乐史,其他课请自行抓包
+    rec = r2.text
+    i += 1
+    print rec, i
+    if rec.find("flag\":true") > 0:
+        yes = True
+    time.sleep(0.05)
+
+
 while True:
     try:
-        r2 = s.get("http://10.0.40.216/bgsxk/xk/processXk?jx0502id=50&jx0404id=201520161002095&jx0502zbid=97", timeout=5)
-        # 西方音乐史,其他课请自行抓包
-        rec = r2.text
-        print rec, i
-        if rec.find("flag\":true") > 0:
+        shua("http://10.0.40.216/bgsxk/xk/processXk?jx0502id=50&jx0404id=201520161002740&jx0502zbid=97")
+        shua("http://10.0.40.216/bgsxk/xk/processXk?jx0502id=50&jx0404id=201520161002775&jx0502zbid=97")
+        if yes:
             break
-        time.sleep(0.05)
-        i += 1
     except:
         pass
+
